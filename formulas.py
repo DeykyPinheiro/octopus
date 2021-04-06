a360596ab763808abbf2bcc0802c2c5c4ad11c3c
Deyky Pinheiro <pinheiromikael96@gmail.com>
2021-04-05 21:14:06 -0300

    parametros padroes, concerto de periodos nas formulas

--- formulas.py
+++ formulas.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 #medias moveis, padrao 20
 def medias_moveis(df, periodos=20):
-    df[f'mean_{periodos}'] = df['Close'].rolling(window=20).mean()
+    df[f'mean_{periodos}'] = df['Close'].rolling(window=periodos).mean()
 

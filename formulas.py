f2d1bada68ece87ef21593af038e994e75a45036
Deyky Pinheiro <pinheiromikael96@gmail.com>
2021-04-09 17:30:50 -0300

    adicionei funcao ATR

--- formulas.py
+++ formulas.py
@@ -23,5 +23,22 @@ def macd(df, curta=12, longa=26, sinal=9):
     df_macd[f'mme_macd_{sinal}'] = media_exponencial_deslizante(df_macd, sinal)
     return  df_macd
 
-#link de ajuda
-#https://www.investimentonabolsa.com/2015/03/analise-tecnica-average-true-range-atr.html
+# Recebe um df e os periodos da media exponencial, e os periodos, e devolve um df com o ATR
+# recebe apenas no padrao 'High', 'Low', 'Close'
+def atr(df, periodos=14):
+    
+    data = df.copy()
+
+    high = data['High']
+    low = data['Low']
+    close = data['Close']
+
+    data['tr0'] = abs(high - low)
+    data['tr1'] = abs(high - close.shift(1))
+    data['tr2'] = abs(low - close.shift(1))
+    
+    tr = data[['tr0', 'tr1', 'tr2']].max(1)
+
+    atr = media_exponencial_deslizante(tr, periodos)
+    
+    return atr
